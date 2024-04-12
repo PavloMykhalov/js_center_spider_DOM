ad5c8b147c95df571557212bc61de8cb3985b19e
Pavlo Mykhalov <pavlomihaliov@gmail.com>
2024-04-12 14:52:43 +0300

    make build

---
+++ index.30d6b931.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=(e.clientHeight-t.clientWidth)/2,l=(e.clientHeight-t.clientHeight)/2;t.style.left="".concat(c,"px"),t.style.top="".concat(l,"px");
+//# sourceMappingURL=index.30d6b931.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const spiderContainer = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const leftPosition = (spiderContainer.clientHeight - spider.clientWidth) / 2;
+const topPosition = (spiderContainer.clientHeight - spider.clientHeight) / 2;
+spider.style.left = `${leftPosition}px`;
+spider.style.top = `${topPosition}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
